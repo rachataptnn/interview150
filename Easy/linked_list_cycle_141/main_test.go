064c2f970077a2689e809f9c7056a6e2c48dbfad
rachata.p@kbtg.tech <rachata.ptnn@gmail.com>
2023-07-25 08:27:12 +0700

    move 141 ll cycle to easy

---
+++ Easy/linked_list_cycle_141/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRemoveDupII(t *testing.T) {
+	type testcaseStruct struct {
+		casename string
+		input    []int
+		pos      int
+		expected bool
+	}
+
+	testcases := []testcaseStruct{
+		{
+			casename: "case 1",
+			input:    []int{3, 2, 0, -4},
+			pos:      1,
+			expected: true,
+		},
+		{
+			casename: "case 2",
+			input:    []int{1, 2},
+			pos:      0,
+			expected: true,
+		},
+		{
+			casename: "case 3",
+			input:    []int{1},
+			pos:      -1,
+			expected: false,
+		},
+	}
+
+	for _, tc := range testcases {
+		actual := hasCycle(createLinkedList(tc.input))
+
+		if !reflect.DeepEqual(actual, tc.expected) {
+			t.Errorf("testcase: %s - Expected %v, but got %v", tc.casename, tc.expected, actual)
+		}
+	}
+}
